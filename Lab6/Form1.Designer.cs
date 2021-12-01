0cf6119e6a9523d5760ee59c5d8f04963f4bec19
Zach <Zach@ZACHS-PC>
2021-12-01 15:05:40 -0600

    Add project files.

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Lab6
+{
+    partial class TemperatureConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numberboxTempValue = new System.Windows.Forms.NumericUpDown();
+            this.comboUnits = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageTempGauge = new System.Windows.Forms.PictureBox();
+            this.lblConversion1Value = new System.Windows.Forms.Label();
+            this.lblConversion2Value = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTempGauge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberboxTempValue
+            // 
+            this.numberboxTempValue.Location = new System.Drawing.Point(236, 16);
+            this.numberboxTempValue.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.numberboxTempValue.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numberboxTempValue.Minimum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            -2147483648});
+            this.numberboxTempValue.Name = "numberboxTempValue";
+            this.numberboxTempValue.Size = new System.Drawing.Size(167, 26);
+            this.numberboxTempValue.TabIndex = 0;
+            this.numberboxTempValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numberboxTempValue.ValueChanged += new System.EventHandler(this.numberboxTempValue_ValueChanged);
+            // 
+            // comboUnits
+            // 
+            this.comboUnits.FormattingEnabled = true;
+            this.comboUnits.Items.AddRange(new object[] {
+            "Fahrenheit",
+            "Celsius",
+            "Kelvin"});
+            this.comboUnits.Location = new System.Drawing.Point(413, 15);
+            this.comboUnits.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comboUnits.Name = "comboUnits";
+            this.comboUnits.Size = new System.Drawing.Size(143, 26);
+            this.comboUnits.TabIndex = 1;
+            this.comboUnits.TextChanged += new System.EventHandler(this.comboUnits_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Starting Temperature:";
+            // 
+            // imageTempGauge
+            // 
+            this.imageTempGauge.Image = global::Lab6.Properties.Resources.temperaturegauge_none;
+            this.imageTempGauge.Location = new System.Drawing.Point(12, 81);
+            this.imageTempGauge.Name = "imageTempGauge";
+            this.imageTempGauge.Size = new System.Drawing.Size(214, 73);
+            this.imageTempGauge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageTempGauge.TabIndex = 3;
+            this.imageTempGauge.TabStop = false;
+            // 
+            // lblConversion1Value
+            // 
+            this.lblConversion1Value.BackColor = System.Drawing.Color.White;
+            this.lblConversion1Value.Location = new System.Drawing.Point(236, 81);
+            this.lblConversion1Value.Name = "lblConversion1Value";
+            this.lblConversion1Value.Size = new System.Drawing.Size(320, 30);
+            this.lblConversion1Value.TabIndex = 4;
+            this.lblConversion1Value.Text = "value 1";
+            this.lblConversion1Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblConversion2Value
+            // 
+            this.lblConversion2Value.BackColor = System.Drawing.Color.White;
+            this.lblConversion2Value.Location = new System.Drawing.Point(236, 124);
+            this.lblConversion2Value.Name = "lblConversion2Value";
+            this.lblConversion2Value.Size = new System.Drawing.Size(320, 30);
+            this.lblConversion2Value.TabIndex = 5;
+            this.lblConversion2Value.Text = "value 2";
+            this.lblConversion2Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TemperatureConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 169);
+            this.Controls.Add(this.lblConversion2Value);
+            this.Controls.Add(this.lblConversion1Value);
+            this.Controls.Add(this.imageTempGauge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboUnits);
+            this.Controls.Add(this.numberboxTempValue);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "TemperatureConverter";
+            this.Text = "Temperature Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTempGauge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numberboxTempValue;
+        private System.Windows.Forms.ComboBox comboUnits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox imageTempGauge;
+        private System.Windows.Forms.Label lblConversion1Value;
+        private System.Windows.Forms.Label lblConversion2Value;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+
+Author: Zachary Salvaggio
+
+Program Title: Temperature Converter
+
+File Description: Lab 6, Final Project for CSC 317
+
+This program takes a temperature in kelvin, celsius, or fahrenheit and converts it. It also has a temperature gauge which is automatically updated. 
+
+*/
+
+namespace Lab6
+{
+    public partial class TemperatureConverter : Form
+    {
+
+        private decimal fahrenheit_gauge = 9999999;
+        public TemperatureConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberboxTempValue_ValueChanged(object sender, EventArgs e)
+        {
+
+            decimal entered = numberboxTempValue.Value;
+
+            if (comboUnits.Text != null)
+            {
+                switch (comboUnits.Text)
+                {
+                    case "Fahrenheit":
+                        //convert from farenheit to celsius and kelvin
+                        if(entered <= -459.67m) { displayWarning();return; }
+                        lblConversion1Value.Text = convertToCelsius('F', entered).ToString("0.00") + "°C";
+                        lblConversion2Value.Text = convertToKelvin('F', entered).ToString("0.00") + "K";
+                        fahrenheit_gauge = entered;
+                        break;
+                    case "Celsius":
+                        //convert from celsius to fahrenheit and kelvin
+                        if (entered <= -273.15m) { displayWarning(); return; }
+                        lblConversion1Value.Text = convertToFahrenheit('C', entered).ToString("0.00") + "°F";
+                        lblConversion2Value.Text = convertToKelvin('C', entered).ToString("0.00") + "K";
+                        break;
+                    case "Kelvin":
+                        //convert from kelvin to celsius and fahrenheit
+                        if (entered <= 0m) { displayWarning();return; }
+                        lblConversion1Value.Text = convertToFahrenheit('K', entered).ToString("0.00") + "°F";
+                        lblConversion2Value.Text = convertToCelsius('K', entered).ToString("0.00") + "°C";
+                        break;
+                }
+            }
+        }
+
+        private decimal convertToKelvin(char startingTemp, decimal value)
+        {
+            decimal kelvin = 0;
+
+            switch (startingTemp)
+            {
+                case 'F':
+                    kelvin = (value + 459.67m) * (5m / 9m);
+                    break;
+                case 'C':
+                    kelvin = value + 273.15m;
+                    break;
+            }
+
+            return kelvin;
+
+        }
+
+        private decimal convertToCelsius(char startingTemp, decimal value)
+        {
+            decimal celsius = 0;
+
+            switch (startingTemp)
+            {
+                case 'F':
+                    celsius = (value - 32m) * (5m / 9m);
+                    break;
+                case 'K':
+                    celsius = value - 273.15m;
+                    break;
+            }
+
+            return celsius;
+        }
+
+        private decimal convertToFahrenheit(char startingTemp, decimal value)
+        {
+            decimal fahrenheit = 0;
+
+            switch (startingTemp)
+            {
+                case 'C':
+                    fahrenheit = (value * (9m / 5m)) + 32;
+                    break;
+                case 'K':
+                    fahrenheit = (value - 273.15m) * (9m / 5m) + 32;
+                    break;
+            }
+
+            fahrenheit_gauge = fahrenheit;
+            return fahrenheit;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            if (fahrenheit_gauge > 95m && fahrenheit_gauge != 9999999)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_hot;
+            }
+            else if (fahrenheit_gauge <= 95m && fahrenheit_gauge > 75m)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_warm;
+            }
+            else if (fahrenheit_gauge <= 75m && fahrenheit_gauge > 55m)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_mild;
+            }
+            else if (fahrenheit_gauge <= 55m && fahrenheit_gauge > 33m)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_cold;
+            }
+            else if (fahrenheit_gauge < 33m)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_freezing;
+            }
+            else if (fahrenheit_gauge == 9999999)
+            {
+                imageTempGauge.Image = Properties.Resources.temperaturegauge_none;
+            }
+
+        }
+
+        private void comboUnits_TextChanged(object sender, EventArgs e)
+        {
+            numberboxTempValue_ValueChanged(sender, e);
+        }
+
+        private void displayWarning()
+        {
+            MessageBox.Show("The value you entered is invalid. Please enter a valid temperature");
+        }
+    }
+}
